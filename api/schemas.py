37b6749258cafd0a12c28d40f9bca219ad980fe1
Tiago Guimaraes <tiagofaguimaraes@gmail.com>
2024-09-16 21:20:42 -0300

    feat: create context, session and message schemas

---
+++ api/schemas.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+
+
+class ChatMessageBase(BaseModel):
+    content : str
+    is_bot_message : bool
+
+class ChatMessageCreate(ChatMessageBase):
+    pass
+
+class ChatMessage(ChatMessageBase):
+    id : str
+    session_id : str
+
+    class Config:
+        orm_mode = True
+
+
+class ChatSessionBase(BaseModel):
+    user_name : str
+    context_id : str
+
+class ChatSessionCreate(ChatSessionBase):
+    pass
+
+class ChatSession(ChatSessionBase):
+    id : str
+    is_active : bool
+    messages : list[ChatMessage] = []
+
+    class Config:
+        orm_mode = True
+
+
+class ChatContextBase(BaseModel):
+    name : str
+
+class ChatContextCreate(ChatContextBase):
+    pass
+
+class ChatContext(ChatContextBase):
+    id : str
+    sessions : list[ChatSession] = []
+
+    class Config:
+        orm_mode = True
