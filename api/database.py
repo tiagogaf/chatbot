26836d852d619622aa916a418c559bf9f1c5f4f0
Tiago Guimaraes <tiagofaguimaraes@gmail.com>
2024-09-17 11:27:40 -0300

    refactor: code improvement in the database engine

--- api/database.py
+++ api/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-import os
-DB_URL = DB_URL = os.getenv("DB_URL")
+
+DB_URL = os.getenv("DB_URL")
 connect_args={'ssl':{'fake_flag_to_enable_tls': True}}
 engine = create_engine(DB_URL,echo=True, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
